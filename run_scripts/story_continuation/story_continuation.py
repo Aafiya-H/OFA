c3dc1598f4bf3d691e7a9e987adf39ebc7d0cf71
Aafiya-H <aafiya.hussain2@gmail.com>
2023-07-19 08:10:31 +0000

    updated story continuation

--- run_scripts/story_continuation/story_continuation.py
+++ run_scripts/story_continuation/story_continuation.py
@@ -1,3 +1,19 @@
 from fairseq import tasks
+from tasks.mm_tasks import VisualStorytelling
 
-def 
+import argparse
+import torch
+
+tasks.register_task("visual_storytelling",VisualStorytelling)
+use_cuda = torch.cuda.is_available()
+use_fp16 = True if use_cuda else False
+
+def main(args):
+  pass
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("image_path", type=str, help="Path to the image")
+  parser.add_argument("text", type=str, text="Image description")
+  args = parser.parse_args()
+  main(args)
